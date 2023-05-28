ec7fbf80b19ad58bfe4a9c4863a2ba5679376730
Anastasia <nastay.dr2002@gmail.com>
2023-05-28 20:56:44 +0400

    Добавила эндпоинты "Удалить объявление" и "Обновить информацию об объявлении" в AdsController

--- src/main/java/ru/skypro/ads/service/impl/AdsServiceImpl.java
+++ src/main/java/ru/skypro/ads/service/impl/AdsServiceImpl.java
@@ -2,7 +2,8 @@ package ru.skypro.ads.service.impl;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import ru.skypro.ads.dto.AdsDto;
+import ru.skypro.ads.dto.Ads;
+import ru.skypro.ads.dto.CreateAds;
 import ru.skypro.ads.service.AdsService;
 
 import java.util.Collection;
@@ -10,22 +11,62 @@ import java.util.Collection;
 @Service
 public class AdsServiceImpl implements AdsService {
 
-
+    /**
+     * Получение всех объявлений
+     *
+     * @return коллекция всех объектов {@link Ads}
+     */
     @Override
-    public Collection<AdsDto> getAllAds() {
+    public Collection<Ads> getAllAds() {
         return null;
     }
 
+    /**
+     * Добавление объявления
+     *
+     * @param ads
+     * @param name
+     * @param image объект {@link MultipartFile}
+     * @return объект {@link Ads}
+     */
     @Override
-    public AdsDto save(Object ads, String name, MultipartFile image) {
+    public Ads save(Object ads, String name, MultipartFile image) {
         return null;
     }
+
+    /**
+     * Получает информацию об объявлении
+     *
+     * @param id идентификатор объявления
+     * @return объект {@link Ads}
+     */
     @Override
-    public AdsDto getAds(Integer id) {
+    public Ads getAd(Integer id) {
 //        AdsDto ads = adsRepository.findById(id).orElseThrow();
 //        return ads;
         return null;
     }
 
+    /**
+     * Удаляет объявление
+     *
+     * @param id идентификатор объявления
+     * @return <code>true</code> если объявление удалено, <code>false</code> в случае неудачи
+     */
+    @Override
+    public boolean deleteAd(int id) {
+        return false;
+    }
 
+    /**
+     * Обновляет информацию об объявлении
+     *
+     * @param id        идентификатор объявления
+     * @param createAds новая информация об объявлении
+     * @return <code>true</code> если объявление обновлено, <code>false</code> в случае неудачи
+     */
+    @Override
+    public boolean updateAd(int id, CreateAds createAds) {
+        return false;
+    }
 }
