3c36f094c0bdd04bc1b71c31fdc0cb4d20090aae
Anastasia <nastay.dr2002@gmail.com>
2023-06-15 20:57:20 +0400

    Добавила /ads в AUTH_WHITELIST

--- src/main/java/ru/skypro/ads/WebSecurityConfig.java
+++ src/main/java/ru/skypro/ads/WebSecurityConfig.java
@@ -23,7 +23,8 @@ public class WebSecurityConfig {
             "/v3/api-docs",
             "/webjars/**",
             "/login",
-            "/register"
+            "/register",
+            "/ads"
     };
 
 //  @Bean
